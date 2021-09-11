afd075b7a1f47b11d219b45296cec8df9fb58ed5
Evgenii Terskikh <eterskikh@nvidia.com>
2021-09-11 15:54:20 +0300

    My 3rd attempt for sprint #2 project

--- homework.py
+++ homework.py
@@ -5,6 +5,9 @@ import datetime as dt
 # depending on whether date was provided or not.
 # Made it as a function, which I call in different parts
 # of the code to get current date.
+# I left it like that just in case someone in the future decide to use
+# the function without providing the date. In other case
+# date_provided=dt.datetime.now().strftime('%d.%m.%Y') might be deleted.
 def current_date(date_provided=dt.datetime.now().strftime('%d.%m.%Y')):
     return dt.datetime.strptime(date_provided, '%d.%m.%Y').date()
 
@@ -102,22 +105,3 @@ class Record:
         self.date = date
         self.amount = amount
         self.comment = comment
-
-
-# создадим калькулятор денег с дневным лимитом 1000
-cash_calculator = CashCalculator(1000)
-
-# дата в параметрах не указана,
-# так что по умолчанию к записи
-# должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment='кофе'))
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000,
-                                  comment='бар в Танин др',
-                                  date='08.11.2019'))
-
-print(cash_calculator.get_today_cash_remained('rub'))
-# должно напечататься
-# На сегодня осталось 555 руб
